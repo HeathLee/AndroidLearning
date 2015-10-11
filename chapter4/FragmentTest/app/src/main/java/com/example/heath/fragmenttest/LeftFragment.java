0d16869d8ba8d2f2451db8a3d3906f8ab6c84770
HeathLee <753940818@qq.com>
2015-10-11 16:39:31 +0800

    初级fragment使用

---
+++ chapter4/FragmentTest/app/src/main/java/com/example/heath/fragmenttest/LeftFragment.java
@@ -0,0 +1,21 @@
+package com.example.heath.fragmenttest;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by heath on 15-10-10.
+ */
+public class LeftFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.left_fragment, container, false);
+        return view;
+    }
+}
