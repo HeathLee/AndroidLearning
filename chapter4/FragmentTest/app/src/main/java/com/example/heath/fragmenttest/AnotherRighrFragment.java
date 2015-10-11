d54c41c209fe5fec368ef2d2df5be9241e83b8a5
HeathLee <753940818@qq.com>
2015-10-11 17:09:23 +0800

    动态添加Fragment

---
+++ chapter4/FragmentTest/app/src/main/java/com/example/heath/fragmenttest/AnotherRighrFragment.java
@@ -0,0 +1,22 @@
+package com.example.heath.fragmenttest;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by heath on 15-10-11.
+ */
+public class AnotherRighrFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.another_right_fragment,
+                container, false);
+        return view;
+    }
+}
--- chapter4/FragmentTest/app/src/main/java/com/example/heath/fragmenttest/MainActivity.java
+++ chapter4/FragmentTest/app/src/main/java/com/example/heath/fragmenttest/MainActivity.java
@@ -1,16 +1,34 @@
 package com.example.heath.fragmenttest;
 
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AnotherRighrFragment anotherRighrFragment = new
+                        AnotherRighrFragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager
+                        .beginTransaction();
+                fragmentTransaction.replace(R.id.right_layout,
+                        anotherRighrFragment);
+                fragmentTransaction.commit();
+            }
+        });
     }
 
     @Override
