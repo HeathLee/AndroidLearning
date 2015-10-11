7ed48d411366b066d35b95e5fadc5de4c5587f1a
HeathLee <753940818@qq.com>
2015-10-12 01:25:11 +0800

    Fragment生命周期； 调整到Android5.0

--- chapter4/FragmentTest/app/src/main/java/com/example/heath/fragmenttest/MainActivity.java
+++ chapter4/FragmentTest/app/src/main/java/com/example/heath/fragmenttest/MainActivity.java
@@ -1,15 +1,15 @@
 package com.example.heath.fragmenttest;
 
-import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends Activity{
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- chapter4/FragmentTest/app/src/main/java/com/example/heath/fragmenttest/RightFragment.java
+++ chapter4/FragmentTest/app/src/main/java/com/example/heath/fragmenttest/RightFragment.java
@@ -1,8 +1,10 @@
 package com.example.heath.fragmenttest;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,11 +13,75 @@ import android.view.ViewGroup;
  * Created by heath on 15-10-10.
  */
 public class RightFragment extends Fragment {
+    public static final String TAG = "RightFragment";
+
+    @Override
+    public void onAttach(Activity context) {
+        super.onAttach(context);
+        System.out.println("Test");
+        Log.d(TAG, "onAttach");
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView");
         View view = inflater.inflate(R.layout.right_fragment, container, false);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.d(TAG, "onActivityCreated");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(TAG, "onDestroyView");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d(TAG, "onDetach");
     }
 }
