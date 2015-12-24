23c4261abc2c4e9080ca9c2c786a9f49a56d2936
HeathLee <753940818@qq.com>
2015-12-24 18:53:12 +0800

    finish

--- class_homework/HW-9/app/src/main/java/com/example/heath/hw_9/Download.java
+++ class_homework/HW-9/app/src/main/java/com/example/heath/hw_9/Download.java
@@ -16,10 +16,6 @@ public class Download implements Runnable {
 
     private Handler handler;
 
-    public Download(Handler handler) {
-        this.handler = handler;
-    }
-
     private final static String NAMESPACE = "http://WebXml.com.cn/";
     private final static String METHODNAME = "enValidateByte";
     private final static String SOAPACTION = "http://WebXml.com" +
@@ -27,7 +23,9 @@ public class Download implements Runnable {
     private final static String URL = "http://webservice.webxml.com" +
             ".cn/WebServices/ValidateCodeWebService.asmx";
 
-    private final static int UPDATE = 1, FAILED = 2;
+    public Download(Handler handler) {
+        this.handler = handler;
+    }
 
     @Override
     public void run() {
@@ -37,21 +35,21 @@ public class Download implements Runnable {
                 (SoapEnvelope.VER10);
         envelope.dotNet = true;
         envelope.setOutputSoapObject(request);
-        HttpTransportSE transportSE = new HttpTransportSE(URL);
+        HttpTransportSE transportSE = new HttpTransportSE(URL, 80000);
         try {
             transportSE.call(SOAPACTION, envelope);
             SoapObject result = (SoapObject)envelope.bodyIn;
             SoapPrimitive detail = (SoapPrimitive)result.getProperty
                     ("enValidateByteResult");
             Message msg = new Message();
-            msg.what = UPDATE;
+            msg.what = MainActivity.UPDATE;
             msg.obj = detail;
             handler.sendMessage(msg);
         } catch (Exception e) {
             e.printStackTrace();
             Message msg = new Message();
-            msg.what = FAILED;
-            msg.obj = null;
+            msg.what = MainActivity.FAILED;
+            msg.obj = e.toString();
             handler.sendMessage(msg);
         }
     }
--- class_homework/HW-9/app/src/main/java/com/example/heath/hw_9/MainActivity.java
+++ class_homework/HW-9/app/src/main/java/com/example/heath/hw_9/MainActivity.java
@@ -8,18 +8,24 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final static int UPDATE = 1, FAILED = 2;
-
+    public final static int UPDATE = 1, FAILED = 2;
+    private static final String BASE =
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     private EditText mStrEditText;
     private Button mButtonButton;
     private ImageView mResultImageView;
+    private TextView mValidateResult, mErrorInfo;
     static String str;
     private ProgressDialog dialog;
 
@@ -30,17 +36,34 @@ public class MainActivity extends AppCompatActivity {
         mStrEditText = (EditText) findViewById(R.id.str);
         mButtonButton = (Button) findViewById(R.id.button);
         mResultImageView = (ImageView) findViewById(R.id.result);
+        mValidateResult = (TextView) findViewById(R.id.validate_result);
+        mErrorInfo = (TextView) findViewById(R.id.error_info);
 
         mButtonButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                str = mStrEditText.getText().toString();
-                dialog = new ProgressDialog(MainActivity.this);
-                dialog.setCancelable(false);
-                dialog.show();
-                new Thread(new Download(handler)).start();
+//                mErrorInfo.setText("");
+//                str = getRandomStr(4);
+//                dialog = new ProgressDialog(MainActivity.this);
+//                dialog.setCancelable(false);
+//                dialog.show();
+//                new Thread(new Download(handler)).start();
+                Log.d("xyz", str);
+                Log.d("xyz", mStrEditText.getText().toString());
+                if (str.equals(mStrEditText.getText().toString())) {
+                    mValidateResult.setText("验证码输入正确");
+                } else {
+                    mValidateResult.setText("验证码输入错误，请重新输入");
+                    getValidateCodeImage();
+                }
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getValidateCodeImage();
     }
 
     private Handler handler = new Handler() {
@@ -58,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                     break;
                 case FAILED:
+                    mErrorInfo.setText(msg.obj.toString());
                     if (dialog != null) {
                         dialog.dismiss();
                     }
@@ -65,4 +89,21 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+
+    private String getRandomStr(int length) {
+        Random random = new Random();
+        StringBuffer buffer = new StringBuffer();
+        for (int i = 0; i < length; ++i) {
+            buffer.append(BASE.charAt(random.nextInt(62)));
+        }
+        return buffer.toString();
+    }
+
+    private void getValidateCodeImage() {
+        str = getRandomStr(4);
+        dialog = new ProgressDialog(MainActivity.this);
+        dialog.setCancelable(false);
+        dialog.show();
+        new Thread(new Download(handler)).start();
+    }
 }
